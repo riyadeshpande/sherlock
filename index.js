1c94685d54819dbd2d42912311a39e63c2dcd824
Riya Deshpande <88222224+riyadeshpande@users.noreply.github.com>
2023-10-11 17:19:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log(document.cookie)
+//document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+document.cookie = "click_here=aHR0cDovL2JpdC5seS8zTFZ3TlVW"
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/',function(req, res) {
+//     const ipAddress = req.socket.remoteAddress;
+//     res.send(ipAddress);
+// });
+
+// app.listen(3000, () => console.log(`Server is listening on port 3000`))
+
+// let key = prompt("Enter your key")
+// let value = prompt("Enter your value")
+// document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+// console.log(document.cookie)
